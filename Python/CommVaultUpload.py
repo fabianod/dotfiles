6b27c1d0436e04656b5f62ce0c18ca8568db1146
Ryan D'souza <dsouzarc@gmail.com>
2015-12-05 13:41:01 -0500

    Major comments + deletions + code reorganization + spacing

--- Python/CommVaultUpload.py
+++ Python/CommVaultUpload.py
@@ -7,6 +7,20 @@ import json;
 
 import config;
 
+######################################################################################
+# Written by Ryan D'souza
+# Quickly uploads files and directories to my cloud storage
+# Very light weight, uses standard Python libraries, only requires a password
+#
+# For run instructions, see
+#   https://github.com/dsouzarc/dotfiles/tree/master/Python#commvault-uploader
+######################################################################################
+
+
+####################################
+####        BASE URLS           ####
+####################################
+
 server = "cloud.commvault.com";
 baseURL = "https://" + server + "/webconsole/";
 mainAPIURL = baseURL + "api/";
@@ -110,16 +124,71 @@ def generateHeaders(pathToFile):
 ####     FOR CHUNK UPLOADING    ####
 ####################################
 
+def uploadFileInChunks(pathToFile):
+
+    print("Beginning to upload " + os.path.basename(pathToFile));
+
+    #Gets the request id for the upload
+    requestID, chunkOffset = handleConflict(pathToFile);
+
+    fileSize = os.path.getsize(pathToFile);
+    fileToUpload = open(pathToFile, 'rb');
+
+    #If it's already uploaded - same file size and last modified date
+    if chunkOffset == -1:
+        print("Successfully finished uploading " + os.path.basename(pathToFile));
+        return;
+
+    #Partial upload, start reading from where we left off
+    if chunkOffset != 0:
+        fileToUpload.seek(chunkOffset);
+
+    #Go through the remaining bytes
+    while(chunkOffset + (maxSingleSize) < fileSize):
+
+        #Get the chunk, create our url, and upload it
+        chunk = fileToUpload.read(maxSingleSize);
+        url = uploadChunkedFileRequestIdURL + str(requestID);
+        requestIDOld, chunkResponse = beginChunkUpload(pathToFile, chunk, False, url);
+
+        #Same as above - file has already been uploaded
+        if chunkResponse == -1:
+            print("Successfully finished uploading " + os.path.basename(pathToFile));
+            return;
+        chunkOffset += 4 * 1000 * 1000;
+
+    #The last chunk gets a special tag, so it can't be in the while loop
+    remainder = fileSize - chunkOffset;
+    print("HERE: " + str(fileSize) + "\t" + str(chunkOffset));
+    chunk = fileToUpload.read(remainder);
+
+    #Upload that last chunk
+    url = uploadChunkedFileRequestIdURL + str(requestID);
+    beginChunkUpload(pathToFile, chunk, True, url);
+    print("Successfully finished uploading " + os.path.basename(pathToFile));
+
+
+########################################################################
+####     GETS REQUESTID + LAST CHUNK FOR UPLOADING IN CHUNKS        ####     
+########################################################################
+
 def handleConflict(pathToFile):
 
+    #Create our request and remove excess parameters
     request = urllib2.Request(uploadChunkedFileInitial);
     headers, fileName, cloudPath = generateHeaders(pathToFile);
     headers["FileEOF"] = "0";
     del headers["Content-type"];
     request.headers = headers;
-    request.add_data(open(pathToFile, 'rb').read(4 * 1000 * 1000));
+
+    #Get the first chunk of data
+    request.add_data(open(pathToFile, 'rb').read(maxSingleSize));
+
+    #Try because python throws an exception if the http status isn't 200
     try:
         response = urllib2.urlopen(request);
+
+        #409 = partially completed uploaded. 200 = first upload
         if response.code == 409 or response.code == 200:
             response = json.loads(response.read());
             response = response['DM2ContentIndexing_UploadFileResp'];
@@ -127,10 +196,16 @@ def handleConflict(pathToFile):
             chunkOffset = long(response['@chunkOffset']);
             print("Found duplicate for " + fileName + "\tChunk: " + str(chunkOffset) + str(response));
             return requestID, chunkOffset;
+
+        #Some other http status code
         else:
             print("ERROR: " + str(response));
             return 0, 0;
+
+    #Any exceptions
     except urllib2.HTTPError, err:
+
+        #Handle these the same way
         if err.code == 409 or err.code == 200:
             response = json.loads(err.read());
             response = response['DM2ContentIndexing_UploadFileResp'];
@@ -138,52 +213,19 @@ def handleConflict(pathToFile):
             chunkOffset = long(response['@chunkOffset']);
             print("Found duplicate for " + fileName + "\tChunk: " + str(chunkOffset) + "\t" + str(response));
             return requestID, chunkOffset;
+
         else:
             print("ERROR: " + str(err.read()));
             return 0, 0;
 
-def uploadFileInChunks(pathToFile):
 
-    print("Beginning to upload " + os.path.basename(pathToFile));
-
-    requestID, chunkOffset = handleConflict(pathToFile);
-
-    fileSize = os.path.getsize(pathToFile);
-    fileToUpload = open(pathToFile, 'rb');
-
-    if chunkOffset == -1:
-        print("Successfully finished uploading " + os.path.basename(pathToFile));
-        return;
-
-    if chunkOffset != 0:
-        fileToUpload.seek(chunkOffset);
-
-    while(chunkOffset + (4 * 1000 * 1000) < fileSize):
-        print("Uploading new chunk");
-        chunk = fileToUpload.read(4 * 1000 * 1000);
-        url = uploadChunkedFileRequestIdURL + str(requestID);
-        requestIDOld, chunkResponse = beginChunkUpload(pathToFile, chunk, False, url);
-
-        if chunkResponse == -1:
-            print("Successfully finished uploading " + os.path.basename(pathToFile));
-            return;
-        #chunkOffset += 4 * 1000 * 1000;
-
-    remainder = fileSize - chunkOffset;
-    print("HERE: " + str(fileSize) + "\t" + str(chunkOffset));
-    chunk = fileToUpload.read(remainder);
-
-    if chunk:
-        print("CHUNK IS GOOD");
-    else:
-        print("CHUNK IS BAD");
-
-    url = uploadChunkedFileRequestIdURL + str(requestID);
-    beginChunkUpload(pathToFile, chunk, True, url);
-    print("Successfully finished uploading " + os.path.basename(pathToFile));
+##############################################
+####     DOES THE BULK OF CHUNK UPLOADS  ####
+#############################################
 
 def beginChunkUpload(pathToFile, chunk, isLastChunk, url):
 
+    #Get our headers, remove extraneous headers
     request = urllib2.Request(url);
     headers, fileName, cloudPath = generateHeaders(pathToFile);
 
@@ -194,9 +236,9 @@ def beginChunkUpload(pathToFile, chunk, isLastChunk, url):
     del headers["FileSize"];
     del headers["FileName"];
 
+    #The last chunk has a different tag
     if isLastChunk:
-        print("GOT TO LAST CHUNK");
-        #headers["FileEOF"] = "1";
+        headers["FileEOF"] = "1";
     else:
         headers["FileEOF"] = "0";
 
@@ -207,42 +249,47 @@ def beginChunkUpload(pathToFile, chunk, isLastChunk, url):
         response = urllib2.urlopen(request);
     except urllib2.HTTPError, err:
 
+        #Problematic error codes
         if err.code == 400 or err.code == 403:
             print("IN CHUNK BAD REQUEST FOR " + fileName + "\t" + err.read());
             return 0, 0;
 
+        #Duplicate - already partially uploaded
         elif err.code == 409:
             print("DUPLICATE FOUND: " + requestID + "\t" + str(chunkOffSet) + " RETURNED");
             return requestID, chunkOffSet;
+
+        #Yay for more problems
         else:
             print("ERROR: " + str(err.code) + " OUTPUT: " + err.read());
             raise;
 
+    #Same as above -- more problems
     if response.code != 200:
         print("ERROR CHUNK UPLOADING " + pathToFile + " Response code: " + str(response.code));
         return;
 
+    #No error codes, begin parsing the data
     response = json.loads(response.read());
-    print(response);
-
     response = response['DM2ContentIndexing_UploadFileResp'];
 
     if "@errorCode" in response:
+
+        #Chunk was successfully uploaded
         if long(response['@errorCode']) == 200:
             chunkOffSet = long(response['@chunkOffset']);
             requestID = ""; #response['@requestId'];
             print("Successfully uploaded chunk of " + fileName + "\tpath: " + pathToFile + "\tto cloud location: " + cloudPath + str(response));
             return requestID, chunkOffSet;
 
+    #More error codes
     print("ERROR CHUNK UPLOADING " + pathToFile + " MESSAGE: " + str(response));
     return 0, 0
 
 
-'''
-uploadChunkedFileInitial = mainAPIURL + "drive/file/action/upload?uploadType=chunkedFile";
-uploadedChunkedFileForceStart = mainAPIURL + "drive/file/action/upload?uploadType=chunkedFile&forceRestart=true&requestId=";
-uploadChunkedFileRequestIdURL = @"drive/file/action/upload?uploadType=chunkedFile&requestId=";
-'''
+##############################################
+####     UPLOADS SMALL FILES (< 4MB)     ####
+#############################################
 
 def uploadFile(pathToFile):
 
@@ -252,9 +299,11 @@ def uploadFile(pathToFile):
 
     fileSize = os.path.getsize(pathToFile);
 
+    #File too big, we got to chunk it
     if fileSize > maxSingleSize and 1 == 2:
         uploadFileInChunks(pathToFile);
 
+    #We can handle it here
     else:
         request = urllib2.Request(uploadFullFile);
 
@@ -267,6 +316,7 @@ def uploadFile(pathToFile):
 
         response = urllib2.urlopen(request);
 
+        #Unsuccessful upload for whatever reason
         if response.code != 200:
             print("ERROR UPLOADING " + pathToFile + " Response code: " + str(response.code));
             return;
@@ -284,7 +334,6 @@ def uploadFile(pathToFile):
         print("ERROR UPLOADING " + pathToFile + " MESSAGE: " + str(response));
 
 
-#DELETE HERE
 currentDirectory = str(os.getcwd()) + "/";
 
 #KEEP
